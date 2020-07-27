31471ba036e09dc87fc44785b8e6cc5d4019d8c7
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2020-07-27 19:45:11 -0300

    Random and choice

--- scatter_square.py
+++ scatter_square.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt 
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40)
 
 #Define o titulo do gráfico e nomeia os eixos
 plt.title("Square Numbers", fontsize=24)
@@ -9,5 +12,8 @@ plt.ylabel("Square of value", fontsize=24)
 
 #Define o tamano dos rótulos das marcações
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+#Define o intervalo para cada eixo
+plt.axis([0, 1100, 0,1100000])
 
 plt.show()
