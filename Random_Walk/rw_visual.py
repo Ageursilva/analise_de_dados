060703c7638f2a5f74a91aadfdd89404a6fe37a7
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2020-07-27 20:09:46 -0300

    Update rw_visual.py

--- Random_Walk/rw_visual.py
+++ Random_Walk/rw_visual.py
@@ -2,9 +2,19 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Cria um passeio aleatório a plota os pontos
+# Continua criando novos passeios enquanto o programa estiver ativo 
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+    #Cria um passeio aleatório a plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))   
+    plt.scatter(rw.x_values, rw.y_values,c=point_numbers,cmap=plt.cm.Reds,
+                edgecolors='none', s=15)
+   
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
